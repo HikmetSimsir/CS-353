d4861c9e93638a0328b2f453cc0e54194aed7b64
Giray Akyol <giray.akyol@ug.bilkent.edu.tr>
2022-11-05 18:17:48 +0300

    Fix login

---
+++ helper.php
@@ -0,0 +1,51 @@
+<?php
+function getDatabaseConnection(): mysqli
+{
+
+  $configs = include('config.php');
+// connect database
+  $conn = mysqli_connect($configs["hostname"], $configs["username"], $configs["password"], $configs["database"]);
+// check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  return $conn;
+}
+
+function connToSE(PDO $conn): Closure
+{
+  return function (string $str) use ($conn) {
+    return $conn->quote($str);
+  };
+
+}
+
+function reqLogIn()
+{
+  session_start();
+  if (!isset($_SESSION['uname'])) {
+    echo "Need to login";
+    header("location: ./");
+    exit();
+  }
+  echo '<a href="./LogOut.php">Log Out</a>';
+}
+
+function pqef(string $query, PDO $conn)
+{
+  $stmt = $conn->prepare($query);
+  $stmt->execute();
+  return $stmt;
+}
+
+function println(string $str)
+{
+  print($str);
+  print("\n");
+}
+
+function vardumpln(object $str)
+{
+  var_dump($str);
+  print("\n");
+}
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<?php
+include_once "helper.php";
+session_start();
+$conn = getDatabaseConnection();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $email = $_POST['uname'];
+  $password = $_POST['psw'];
+  try {
+    $sql = "SELECT email, display_name FROM user WHERE email = '$email' AND password = '$password'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $count = mysqli_num_rows($result);
+    if ($count == 1) {
+      // if $_SESSION['uname'] isset then user has succesfully logged in
+      $_SESSION['uname'] = $email;
+      $_SESSION['psw'] = $password;
+    } else {
+      echo "<script type='text/javascript'>alert('Wrong username or password');</script>";
+    }
+  } catch (Exception $e) {
+    echo "<script type='text/javascript'>alert('Wrong username or password');</script>";
+  }
+}
+?>
+<form action="" method="post">
+
+    <p>
+        <label for="uname">Email</label><br> <input type="email" name="uname" id="uname" required>
+    </p>
+    <p>
+        <label for="psw">Password</label><br> <input type="password" name="psw" id="psw" required>
+    </p>
+    <p>
+        <button>Log in</button>
+    </p>
+    <p>
+        <a href="SignUp.php">Not a member? Sign Up</a>
+    </p>
+</form>
+</html>
