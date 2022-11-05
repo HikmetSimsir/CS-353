4519c7c99dfa4d37d7e116acb47722d1e15c9c90
Giray Akyol <giray.akyol@ug.bilkent.edu.tr>
2022-11-05 18:41:11 +0300

    Fix signup

--- SignUp.php
+++ SignUp.php
@@ -1,90 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
 <?php
-
+include_once "helper.php";
 $userExists = false;
 $successfullSignup = false;
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $conn = mysqli_connect("localhost", "root", "", "dbproject");
-    session_start();
-
-
-    $username = $_POST["username"];
-    $password = $_POST["psw"];
+  $conn = getDatabaseConnection();
 
 
-    $checkUserNameExistQuery = "Select * from user where email='$username'";
-    $result = mysqli_query($conn, $checkUserNameExistQuery);
-    $count = mysqli_num_rows($result);
+  $username = $_POST["username"];
+  $password = $_POST["psw"];
+  $dname = $_POST["dname"];
 
 
-    $totalNumberOfUsersQuery = mysqli_query($conn, "Select * from user;");
-    $currentNumberOfUsers = mysqli_num_rows($totalNumberOfUsersQuery);
+  $checkUserNameExistQuery = "Select * from user where email='$username'";
+  $result = mysqli_query($conn, $checkUserNameExistQuery);
+  $count = mysqli_num_rows($result);
 
 
-    // check if user with same name exists
-    if($count == 0) {
+  $totalNumberOfUsersQuery = mysqli_query($conn, "Select * from user;");
+  $currentNumberOfUsers = mysqli_num_rows($totalNumberOfUsersQuery);
 
-        $userid = $currentNumberOfUsers + 1;
-        $newUserInsertionQuery = mysqli_query($conn,"Insert into user values('$username',$userid,'$password',null);" );
-        $userExists=false;
-        $successfullSignup = true;
 
-    }
-    else
-    {
-        $userExists=true;
-    }
+  // check if user with same name exists
+  if ($count == 0) {
+
+    $userid = $currentNumberOfUsers + 1;
+    $newUserInsertionQuery = mysqli_query($conn, "Insert into user values('$username',$userid,'$password','$dname');");
+    $userExists = false;
+    $successfullSignup = true;
+
+  } else {
+    $userExists = true;
+  }
 
 }
 
 ?>
 
+<form action="" method="post">
 
+    <p>
+        <label for="username">Email</label><br> <input type="email" name="username" id="username" required>
+    </p>
+    <p>
+        <label for="psw">Password</label><br> <input type="password" name="psw" id="psw" required>
+    </p>
+    <p>
+        <label for="dname">Display name</label><br> <input type="text" name="dname" id="dname" required>
+    </p>
+    <p>
+        <button>Sign up</button>
+    </p>
+    <p>
+        <a href="/">Already a User? Log in</a>
+    </p>
+</form>
 
+<?php if ($userExists) {
+  echo "<script type='text/javascript'>alert('!!!User with same name exists, Signup Failed!!!');</script>";
+}
+?>
 
-
-
-
-
-
-<div class="container">
-
-    <h1 class="text-center">Signup </h1>
-    <form action="SignUp.php" method="post">
-
-        <div class="form-group">
-            <label for="username">Email</label>
-            <input type="text" class="form-control" id="username"
-                   name="username" aria-describedby="emailHelp">
-        </div>
-
-
-
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control"
-                   id="password" name="psw">
-        </div>
-        </div>
-
-        <button type="submit" class="btn">
-            SignUp
-        </button>
-    </form>
-
-        <a href="Login.php"> Go to the login page   </a>
-</div>
-
-<?php if ($userExists): ?>
-    <em>!!!User with same name exists, Signup Failed!!!</em>
-    <br>
-<?php endif; ?>
-
-<?php if ($successfullSignup): ?>
-    <em>!!!You have succesfully signed up, please login!!!</em>
-    <br>
-<?php endif; ?>
+<?php if ($successfullSignup) {
+  echo "<script type='text/javascript'>alert('!!!You have succesfully signed up, please login!!!');</script>";
+} ?>
+</body>
+</html>
 
 
 
