7eddad440d6ad48823c0aad26748cccd3c82b6f8
Giray Akyol <giray.akyol@ug.bilkent.edu.tr>
2022-11-16 23:09:45 +0300

    makereview checkpoint

--- makeReview.php
+++ makeReview.php
@@ -1,18 +1,23 @@
 <html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+</head>
 <body>
 
 
+<h2>Welcome to The Book Review Page</h2>
 
-<h2>Welcome to The Book Review  Page</h2>
-
-<label for="name">Make  your review below. </label>
+<label for="name">Make your review below. </label>
 
 
-<form method="post" >
+<form method="post">
 
     <div class="container" id='transferMoney'>
-        <label for="fromAccount"><b>Your Review </b></label>
-        <input type="text" placeholder="Enter review" name="bookReview" required height=100 width=200>
+        <label for="fromAccount"><b>Your Review </b></label> <input type="text" placeholder="Enter review"
+                                                                    name="bookReview" required height=100 width=200>
         <br>
         <button type="submit">Submit Review</button>
     </div>
@@ -21,64 +26,53 @@
 </form>
 
 
-<br>
-<br>
-<br>
-<br>
+<br> <br> <br> <br>
 
 <table border="1" align="left">
 
-        <td>Previous Reviews of this book</td>
+    <td>Previous Reviews of this book</td>
 
 
-    <?php
+  <?php
 
 
+  $conn = mysqli_connect("localhost", "root", "", "dbproject");
+  session_start();
+
+  $currentBook = $_GET['bookID'];
+  $listPreviousReviewsQuery = mysqli_query($conn, "Select text from book_review where book_id = '$currentBook'");
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $currentUserName = $_SESSION['uname'];
+    $currentUserID = $_SESSION['user_id'];
+
+    $bookReview = $_POST['bookReview'];
 
 
+    //get count of current number of reviews
+    $numberOfReviewForThisBook = mysqli_num_rows($listPreviousReviewsQuery);
 
-    $conn = mysqli_connect("localhost", "root", "", "dbproject");
-    session_start();
+    // add this book review to the table
+    $addBookReviewQuery = mysqli_query($conn, "insert into book_review values('$currentBook',$numberOfReviewForThisBook+1,$currentUserID,'$bookReview',null,null);");
 
-    $currentBook = $_GET['bookID'];
-    $listPreviousReviewsQuery = mysqli_query($conn, "Select text from book_review where book_id = '$currentBook'");
+  }
 
-    if ( $_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $currentUserName = $_SESSION['uname'];
-        $currentUserID = $_SESSION['user_id'];
-
-        $bookReview = $_POST['bookReview'];
+  $listPreviousReviewsQuery = mysqli_query($conn, "Select text from book_review where book_id = '$currentBook'");
 
 
-        //get count of current number of reviews
-        $numberOfReviewForThisBook = mysqli_num_rows($listPreviousReviewsQuery);
+  while ($row = mysqli_fetch_array($listPreviousReviewsQuery, MYSQLI_ASSOC)) {
 
-        // add this book review to the table
-        $addBookReviewQuery = mysqli_query($conn, "insert into book_review values('$currentBook',$numberOfReviewForThisBook+1,$currentUserID,'$bookReview',null,null);");
+    $currentReview = $row['text'];
+    echo "<tr>
 
-}
-
-    $listPreviousReviewsQuery = mysqli_query($conn, "Select text from book_review where book_id = '$currentBook'");
-
-
-    while ($row = mysqli_fetch_array($listPreviousReviewsQuery, MYSQLI_ASSOC)) {
-
-            $currentReview = $row['text'];
-            echo
-            "<tr> 
     <td>{$currentReview} </td>
    </tr>\n";
 
-        }
+  }
 
 
-
-
-
-
-
-    ?>
+  ?>
 
 </table>
 
--- makeReviewPage.php
+++ makeReviewPage.php
@@ -1,49 +1,111 @@
 <html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+</head>
 <body>
 
 
-
-<h2>Welcome to The Book Review  Page</h2>
+<h2>Welcome to The Book Review Page</h2>
 
 <table border="1" align="center">
     <tr>
-        <td>Book id </td>
         <td>Title</td>
-        <td>Publisher ID</td>
-        <td>Publish Date</td>
+        <td>Publisher Name</td>
+        <td>Author Name</td>
+        <td>Genre Info</td>
 
     </tr>
 
-    <?php
+  <?php
 
-    $conn = mysqli_connect("localhost", "root", "", "dbproject");
-    session_start();
-    $listBooktoReviewQuery = mysqli_query($conn,"Select * from book");
+  $filter_title = $_GET["title"];
+  $filter_author = $_GET["author"];
+  $filter_publisher = $_GET["publisher"];
+  $filter_genre = $_GET["genre"];
+  if (is_null($filter_title)) {
+    $filter_title = "";
+  }
+  if (is_null($filter_author)) {
+    $filter_author = "";
+  }
+  if (is_null($filter_publisher)) {
+    $filter_publisher = "";
+  }
+  if (is_null($filter_genre)) {
+    $filter_genre = "";
+  }
+  $form = '<form method="get">
 
-    while ($row = mysqli_fetch_array($listBooktoReviewQuery,MYSQLI_ASSOC)) {
+    <p>
+        <label for="title">book name</label><br> <input type="text" name="title" id="title" value="' . "$filter_title" . '">
+    </p>
 
-        $bookid = $row['book_id'];
-        $currentUserName = $_SESSION['uname'];
-        $userid_from_username_query = mysqli_query($conn,"Select user_id from user where email = '$currentUserName'");
-        $qres = mysqli_fetch_array($userid_from_username_query,MYSQLI_ASSOC);
-        $currentUserID = $qres['user_id'];
-        $_SESSION['user_id'] = $currentUserID;
+    <p>
+        <label for="author">author</label><br> <input type="text" name="author" id="author" value="' . "$filter_author" . '">
+    </p>
 
-        echo
-        "<tr> 
-    <td>{$row['book_id']} </td>
+    <p>
+        <label for="publisher">publisher</label><br> <input type="text" name="publisher" id="publisher" value="' . "$filter_publisher" . '">
+    </p>
+
+    <p>
+        <label for="genre">genre</label><br> <input type="text" name="genre" id="genre" value="' . "$filter_genre" . '">
+    </p>
+    <p>
+        <button>Filter</button>
+    </p>
+
+</form>';
+  echo $form;
+  include_once "helper.php";
+  $conn = getDatabaseConnection();
+  session_start();
+  $sql = "
+select title, publisher.p_name, author.name, genre.genre_info
+from book,
+     book_author,
+     publisher,
+     author,
+     genre,
+     book_genre
+where book.book_id = book_author.book_id
+  and book_author.author_id = author.author_id
+  and book.book_id = book_genre.book_id
+  and book_genre.genre_id = genre.genre_id
+  and book.publisher_id = publisher.p_id
+  and author.name like '%$filter_author%'
+  and publisher.p_name like '%$filter_publisher%'
+  and genre.genre_info like '%$filter_genre%'
+  and book.title like '%$filter_title%'
+";
+  $listBooktoReviewQuery = mysqli_query($conn, $sql);
+
+  while ($row = mysqli_fetch_array($listBooktoReviewQuery, MYSQLI_ASSOC)) {
+
+    $bookid = $row['book_id'];
+    $currentUserName = $_SESSION['uname'];
+    $userid_from_username_query = mysqli_query($conn, "Select user_id from user where email = '$currentUserName'");
+    $qres = mysqli_fetch_array($userid_from_username_query, MYSQLI_ASSOC);
+    $currentUserID = $qres['user_id'];
+    $_SESSION['user_id'] = $currentUserID;
+
+    echo "<tr> 
     <td>{$row['title']}</td>
-    <td>{$row['publisher_id']}</td>
-    <td>{$row['publish_date']}</td>
+    <td>{$row['p_name']}</td>
+    <td>{$row['name']}</td>
+    <td>{$row['genre_info']}</td>
     <td><a href='makeReview.php?bookID=$bookid userID=$currentUserID'>'Make Review to this Book'</a></td>
 
    </tr>\n";
 
-    }
+  }
 
 
-
-    ?>
+  ?>
 
 </table>
 
