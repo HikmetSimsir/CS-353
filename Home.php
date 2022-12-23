f189f5df6288a4361e534b8719e11448adfb7854
Giray Akyol <giray.akyol@ug.bilkent.edu.tr>
2022-12-23 22:30:30 +0300

    used admin author views

--- Home.php
+++ Home.php
@@ -7,12 +7,14 @@
     <link rel="stylesheet" href="./styles.css">
 </head>
 
-<a href="./makeReviewPage.php?title=&author=&publisher=&genre=">Make Review  </a>
-<a href="./ListUsersPage.php?"> Follow Other Users</a>
-<a href = "./followedUsersPage.php?"> Followed Users   </a>
-<a href = "./forumList.php"> Forums   </a>
+<a href="./makeReviewPage.php?title=&author=&publisher=&genre=">Make Review </a> <a href="./ListUsersPage.php?"> Follow
+                                                                                                                 Other
+                                                                                                                 Users</a>
+<a href="./followedUsersPage.php?"> Followed Users </a> <a href="./forumList.php"> Forums </a>
 <?php
-echo '<a href = "./forumCreate.php"> Create Forum</a>';
+if ($_SESSION["isAdmin"]) {
+  echo '<a href = "./forumCreate.php"> Create Forum</a>';
+}
 ?>
 
 
@@ -27,4 +29,28 @@ reqLogIn();
 <p>
     Welcome <?php echo $_SESSION["uname"] ?>
 </p>
+<?php
+if ($_SESSION["isAdmin"]) {
+  echo "<p>Your admin tel no: ";
+  $sql = "SELECT phone_number FROM sys_adm_user WHERE email = '{$_SESSION["uname"]}'";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
+  echo $row["phone_number"];
+  echo "</p>";
+}
+?>
+<?php
+if ($_SESSION["isAuthor"]) {
+  echo "<p>Your author info: ";
+  $sql = "SELECT author_info FROM sys_author_user WHERE email = '{$_SESSION["uname"]}'";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
+  echo $row["author_info"];
+  $sql = "SELECT website_url FROM sys_author_user WHERE email = '{$_SESSION["uname"]}'";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
+  echo "<a href='{$row["website_url"]}'>Website</a>";
+  echo "</p>";
+}
+?>
 </html>
