a859b6d4f12fa9e50db6a138bdf5ac4f6b87e51b
Mustafa Utku Aydoğdu <utku.aydogdu@ug.bilkent.edu.tr>
2022-12-24 22:48:02 +0300

    voting + booklist option

--- makeReview.php
+++ makeReview.php
@@ -46,6 +46,8 @@ navBar($isAdmin, $isAuthor);
     <td>User</td>
     <td>Date</td>
     <td>Your Vote Status</td>
+    <td>Total Upvotes </td>
+    <td>Total Downvotes</td>
 
 
     Please specify your current reading status of this book
@@ -112,6 +114,24 @@ navBar($isAdmin, $isAuthor);
 
   $listPreviousReviewsQuery = mysqli_query($conn, "Select text, rating, user.display_name , review_id, book_review.date from book_review, user where user.user_id = book_review.user_id and book_id = '$currentBook'");
 
+  // get total upvotes and downvotes for each review with current book id
+  $sql = "select count(*) as totalUpvotes, review_id from user_vote_review where review_id in (select review_id from book_review where book_id = '$currentBook') and vote = 'up' group by review_id";
+  $result = mysqli_query($conn, $sql);
+  $upvotes = array();
+
+    while($row = mysqli_fetch_assoc($result)) {
+        $upvotes[$row['review_id']] = $row['totalUpvotes'];
+    }
+
+  // get total upvotes and downvotes for each review with current book id
+  $sql = "select count(*) as totalDownVotes, review_id from user_vote_review where review_id in (select review_id from book_review where book_id = '$currentBook') and vote = 'down' group by review_id";
+  $result = mysqli_query($conn, $sql);
+  $downvotes = array();
+
+  while($row = mysqli_fetch_assoc($result)) {
+      $downvotes[$row['review_id']] = $row['totalDownVotes'];
+  }
+
 
   while ($row = mysqli_fetch_array($listPreviousReviewsQuery, MYSQLI_ASSOC)) {
 
@@ -131,6 +151,17 @@ navBar($isAdmin, $isAuthor);
 
     }
 
+    //get total upvotes and downvotes for this review
+    $totalUpvotes = 0;
+    $totalDownvotes = 0;
+    if (array_key_exists($curReviewID, $upvotes)) {
+        $totalUpvotes = $upvotes[$curReviewID];
+    }
+    if (array_key_exists($curReviewID, $downvotes)) {
+        $totalDownvotes = $downvotes[$curReviewID];
+    }
+
+
       echo "<tr>
 
  
@@ -140,6 +171,9 @@ navBar($isAdmin, $isAuthor);
      <td>{$currentDate} </td>
 
     <td> {$currentVoteStatus} </td>
+    <td> {$totalUpvotes} </td>
+    <td> {$totalDownvotes} </td>
+
     <td> <form method=\"post\" action=\"setVoteStatus.php?status=up&bookID=$currentBook&userID=$currentUserID&reviewID=$curReviewID\"> 
                         <input type='submit' class='button_submit' value='upvote'></form></td>
     <td> <form method=\"post\" action=\"setVoteStatus.php?status=down&bookID=$currentBook&userID=$currentUserID&reviewID=$curReviewID\"> 
